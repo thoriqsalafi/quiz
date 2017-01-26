7a75e9acfdf6decf33cab82b24b64049ba7cef63
thoriqsalafi <thoriq@u.nus.edu>
2017-01-26 10:21:36 +0800

    adding question 2

--- app/src/main/java/com/example/thoriqsalafi/quiz/questionOne.java
+++ app/src/main/java/com/example/thoriqsalafi/quiz/questionOne.java
@@ -1,13 +1,26 @@
 package com.example.thoriqsalafi.quiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class questionOne extends AppCompatActivity {
+
+    Button yesButton, noButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_one);
+
+        yesButton = (Button)findViewById(R.id.yesButton);
+        yesButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(questionOne.this, questionTwo.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/thoriqsalafi/quiz/questionTwo.java
+++ app/src/main/java/com/example/thoriqsalafi/quiz/questionTwo.java
@@ -1,13 +1,27 @@
 package com.example.thoriqsalafi.quiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class questionTwo extends AppCompatActivity {
+
+    Button yesButton, noButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_two);
+
+        noButton = (Button)findViewById(R.id.noButton);
+        noButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(questionTwo.this, questionThree.class);
+                startActivity(i);
+            }
+        });
     }
 }
